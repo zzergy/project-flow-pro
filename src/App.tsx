c82609bc74f21f0da87c39a4d9a4e8627298018f
Plamena <yomena37@gmail.com>
2023-11-03 19:28:49 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Auth/Signup";
+import Login from "./pages/Auth/Login";
+import { ROUTES } from "./utils/enums";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.SIGNUP} element={<Signup />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
