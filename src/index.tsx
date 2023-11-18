6623fe9776684bc0daaf4a90f3e46bffdacab8d2
Plamena <yomena37@gmail.com>
2023-11-18 17:09:22 +0200

    Revert rem to pixels

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { ConfigProvider } from "antd";
 import App from "./App";
+import "./stylingVariables.scss";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(
