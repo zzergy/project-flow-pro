7c3976581445a424885b17e9341cca971202301b
Plamena <yomena37@gmail.com>
2023-11-20 17:49:52 +0200

    Fix error page styling

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import { Button, Result } from "antd";
 import { useNavigate } from "react-router-dom";
-import styles from './ErrorPage.module.scss'
+import styles from "./ErrorPage.module.scss";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
@@ -10,16 +10,22 @@ const ErrorPage = () => {
   };
 
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        <Button type="primary" onClick={handleNavigate} className={styles.button}>
-          Back Home
-        </Button>
-      }
-    />
+    <div className={styles.container}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            type="primary"
+            onClick={handleNavigate}
+            className={styles.button}
+          >
+            Back Home
+          </Button>
+        }
+      />
+    </div>
   );
 };
 
